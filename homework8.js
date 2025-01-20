7e00ad389a283cf7e1c47d0d165427abcc164065
katerina3022 <courseqamanual@gmail.com>
2025-01-20 18:46:38 +0000

    NewChangeForParanoia

---
+++ homework8.js
@@ -0,0 +1,26 @@
+var arr = [
+    {
+        userName: "Test",
+        lastName: "Test",
+        email: "test.test@gmail.com"
+    },
+    {
+        userName: "Dmitro",
+        lastName: "Porohov",
+        email: "dmitro.porohov@yahoo.com"
+    },
+    {
+        userName: "Andrii",
+        lastName: "",
+        email: "andrii@mail.ru" // Нам такі не підходять
+    },
+];
+
+// Регулярний вираз для перевірки email
+var trustedEmailPattern = /^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)*@(gmail\.com|yahoo\.com)$/;
+
+// Отфильтровываем доверенные email
+var trustedEmails = arr.filter(user => trustedEmailPattern.test(user.email)).map(user => user.email);
+
+// Виводим результат
+console.log(trustedEmails);Paranoiaa
